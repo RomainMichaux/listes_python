9d6410931539d0fd4920834da75289e60f69d4e1
RomainMichaux <59529466+RomainMichaux@users.noreply.github.com>
2020-02-04 15:46:59 +0100

    selection améliorer

--- selection.py
+++ selection.py
@@ -6,15 +6,20 @@ for i in range(20):
   liste.append(x)
 print(liste)
 
-minimum = liste[0]
-indice = indice_mini = 0
-for element in liste:
-  if element < minimum:
-    minimum = element
-    indice_mini = indice
-  indice+=1
-print(indice_mini, minimum)
+def minimum(liste):
+  minimum = liste[0]
+  indice = indice_mini = 0
+  for element in liste:
+    if element < minimum:
+      minimum = element
+      indice_mini = indice
+    indice+=1
+  return indice_mini
 
-liste[0], liste[indice_mini] = liste[indice_mini], liste[0]
+print(minimum(liste))
+
+indice_plus_petit = minimum(liste)
+liste[0], liste[indice_plus_petit] = liste[indice_plus_petit], liste[0]
+
 
 print(liste)

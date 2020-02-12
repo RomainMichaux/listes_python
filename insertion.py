0dccde1ff7cac076e8887211636a098350e1459a
RomainMichaux <59529466+RomainMichaux@users.noreply.github.com>
2020-02-12 09:52:35 +0000

    insertion finish

--- insertion.py
+++ insertion.py
@@ -6,38 +6,23 @@ for i in range(20):
   liste.append(x)
 print(liste)
 
-def minimum(liste):
-  minimum = liste[0]
-  indice = indice_mini = 0
-  for element in liste:
-    if element < minimum:
-      minimum = element
-      indice_mini = indice
-    indice+=1
-  return indice_mini
-
-def selection(liste):
-  i =0
-  for i in range(len(liste[i:])):
-    indice_plus_petit = minimum(liste[i:])
-    liste[i], liste[indice_plus_petit+i] = liste[indice_plus_petit+i], liste[i]
-  return liste
-
 def insertion(liste):
-  liste_triee = []
   print(liste)
-  j = 0
-  for element in liste:
-    liste_triee.append(element)
+  liste_triee = []
+  liste_triee.append(liste[0])
+  for element in liste[1:]:
+    print('---------------------------------------')
+    print(f'Element a tester : {element}')
+    dernier = len(liste_triee)-1
     print(liste_triee)
-    i=0
-
-    while (i+2)>= 0 and j>0 and liste_triee[-1-i] < liste_triee[-2-i]:
-      liste_triee[-1-i],liste_triee[-2-i] = liste_triee[-2-i], liste_triee[-1-i]
-      print(f'{j} , {i} : {liste_triee}')
-      i+=1
-    j+=1   
+    print(f'Position du dernier : {dernier}')
+    i=dernier
+    while i>=0 and element < liste_triee[i]:
+      print(f'Pas en {i+1}')
+      i-=1
+    print(f'Placer en  {i+1}')
+    liste_triee.insert(i+1,element)
+    print(f' {liste_triee}')   
   return liste_triee
 
-#print(selection(liste))
 insertion(liste)
